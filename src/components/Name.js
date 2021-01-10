7c9991e2b0592b12483e45cb269dbea7c098892b
Zhaoxian Hu <huzhx@uci.edu>
2021-01-09 23:47:09 -0800

    add Name component

---
+++ src/components/Name.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+export default function Name() {
+  const [name, setName] = useState('');
+
+  return (
+    <labe className="header-name">
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        onClick={(e) => e.target.setSelectionRange(0, e.target.value.length)}
+        placeholder="Untitled"
+      />
+    </labe>
+  );
+}
