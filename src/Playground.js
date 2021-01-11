8a6246b80bb01b02966474b287656a0a323b3d0d
Zhaoxian Hu <huzhx@uci.edu>
2021-01-10 21:15:02 -0800

    learned the other style to implement useCallback

--- src/Playground.js
+++ src/Playground.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react';
 import randomColor from 'randomcolor';
 
 export default function Playground() {
@@ -6,19 +6,31 @@ export default function Playground() {
 
   const inputRef = useRef();
 
-  const [color, setColor] = useState(null);
+  const [color, setColor] = useState(randomColor());
   useEffect(() => {
     setColor(randomColor());
     inputRef.current.focus();
   }, [count]);
+
+  const calculate = useCallback(<Calculate />, [count]);
+
+  const cb = useCallback((num) => console.log(num), [count]);
 
   return (
     <div style={{ borderTop: `10px solid ${color}` }}>
       {count}
       <button onClick={() => setCount((currentCount) => currentCount - 1)}> - </button>
       <button onClick={() => setCount((currentCount) => currentCount + 1)}> + </button>
+      <button onClick={() => setColor(randomColor())}>Change Color</button>
       <hr />
       <input ref={inputRef} type="range" onChange={(e) => setCount(e.target.value)} value={count} />
+      <Calculate cb={cb} num={count} />
     </div>
   );
 }
+
+const Calculate = React.memo(({ cb, num }) => {
+  cb(num);
+  const renderCount = useRef(1);
+  return <div>{renderCount.current++}</div>;
+});
